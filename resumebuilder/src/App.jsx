863cbdc247c4fe2f3aa9d444ec91bcfd2fe4f6ed
thomas <thomas54594@gmail.com>
2026-06-30 18:15:46 +0530

    feat: add routing with react-router-dom and create Loginpage component

--- resumebuilder/src/App.jsx
+++ resumebuilder/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom' 
+import Loginpage from './components-login/Loginpage'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Loginpage />,
+    },
+  ])
+
   return (
     <>
-      
+  
+      <RouterProvider router={router} />
     </>
   )
 }
